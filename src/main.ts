55130960d4f43dd13253705db5f12b99ddf12422
Microseversk <alexander.osipov.work@gmail.com>
2025-03-08 00:15:26 +0700

    update CORS policy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      credentials: true,
+      origin: true,
+    },
+  });
   app.setGlobalPrefix('users');
 
   const config = new DocumentBuilder()
